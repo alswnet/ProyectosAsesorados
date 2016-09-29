92b72c3ea578088398898039ddc223995e849073
chepecarlos <chepecarlos@alswblog.org>
2016-09-29 15:02:03 -0600

    Codigo del braso Terminado

--- BrasoBTServo/BrasoBTServo.ino
+++ BrasoBTServo/BrasoBTServo.ino
@@ -3,7 +3,7 @@
 
 Servo Servo[7];
 
-int PinServo[7] = {3, 4, 5, 6, 7, 10, 11, 12};
+int PinServo[7] = {3, 4, 5, 6, 7, 10, 11};
 
 SoftwareSerial BTSerial(8, 9); // RX | TX Poner los pines que usas
 
@@ -13,8 +13,9 @@ void setup()
     Servo[i].attach(PinServo[i]);
   }
   Serial.begin(9600);
-  Serial.println("Entra en comandos AT:");
+  Serial.println("Iniciando:");
   BTSerial.begin(9600);
+  BTSerial.println("Iniciando");
   //Inicializando los puerto seria de ambos lados
 }
 
@@ -23,17 +24,30 @@ void loop()
   int MoverServo = -1;
   int GradoServo = 0;
   // Leer el puerto HC-05 y lo escrive en el puerto serial de Arduino
-  if (BTSerial.available()) {
+  while (BTSerial.available()) {
 
     char Letra = BTSerial.read();
     Serial.write(Letra);
-    if (Letra >= 'a' && Letra <= 'g') {
-      MoverServo = int(Letra - 'a');
+    if (Letra >= 'a' && Letra <= 'f') {
+      Serial.println("Encontro Letra");
+
+      Serial.println(Letra - 'a');
+      MoverServo = Letra - 'a';
     }
-    if (Letra >= '0' && Letra <= '9') {
+    else if (Letra >= '0' && Letra <= '9') {
       GradoServo = GradoServo * 10 + int(Letra - '0');
     }
-
+    else if (Letra == 'x') {
+      Servo[MoverServo].write(GradoServo);
+      if (MoverServo == 5) {
+        Servo[6].write(GradoServo);
+      }
+      Serial.println();
+      Serial.print("Servo ");
+      Serial.print(MoverServo);
+      Serial.print(" Grado ");
+      Serial.println(GradoServo);
+    }
   }
   // Lee el  puerto serial del arduino y lo escrive en el puerto serial
   if (Serial.available())
