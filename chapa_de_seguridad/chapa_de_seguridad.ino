40c74c25114bb08fededd94bb33eb33820c39422
carl0shdz <carlitos.hernandez203@gmail.com>
2017-06-23 16:33:22 -0300

    segunda prueba con alumnos

--- chapa_de_seguridad/chapa_de_seguridad.ino
+++ chapa_de_seguridad/chapa_de_seguridad.ino
@@ -1,28 +1,52 @@
-#include <Keypad.h>  
+#include <Keypad.h>
 
-const byte Fila = 4; 
-const byte Columna = 3; 
+const byte Fila = 4;
+const byte Columna = 3;
 char llave[Fila][Columna] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
 
-byte PinesFila[Fila] = {5, 4, 3, 2}; 
-byte PinesColumna[Columna] = {8, 7, 6}; 
-char clave[] = {'1','2','3','4','5'};
-char cerradura[] = {'0','0','0','0','0'};
+byte PinesFila[Fila] = {5, 4, 3, 2};
+byte PinesColumna[Columna] = {8, 7, 6};
+char clave[] = {'1', '2', '3', '4', '5'};
+char cerradura[] = {'0', '0', '0', '0', '0'};
 
+int Contador = 0;
+int confirmacion = 0;
 Keypad keypad = Keypad( makeKeymap(llave), PinesFila, PinesColumna, Fila, Columna );
 
-void setup(){
-  Serial.begin(9600); 
+void setup() {
+  Serial.begin(9600);
 }
- 
-void loop(){
-  char key = keypad.getKey(); 
-  if (key != NO_KEY){
-    
+
+void loop() {
+  char key = keypad.getKey();
+  if (key != NO_KEY) {
+    cerradura[Contador] = key;
+    Serial.println(cerradura[Contador]);
+    Contador ++;
+    delay(300);
+  }
+  if (Contador == 5)
+  {
+    Contador = 0;
+    for (int i = 0; i < 5; i++)
+    {
+      if (clave[i] == cerradura[i])
+      {
+        confirmacion++;
+      }
+    }
+    if(confirmacion == 5)
+    {
+      Serial.println("clave correcta");
+    }
+    else
+    {
+      Serial.println("intruso");
+    }
   }
 }
