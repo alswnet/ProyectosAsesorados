07714d41e3374297501bb87612cedbfebfece359
chepecarlos <chepecarlos@alswblog.org>
2017-08-11 21:06:06 -0600

    Sistema Sistema

--- CurcePorCero/CurcePorCero.ino
+++ CurcePorCero/CurcePorCero.ino
@@ -1,21 +1,20 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(11, 12, 5, 4, 3, 2);
+LiquidCrystal lcd(11, 12, 5, 4, 3, 8);
 
-
-const float Dispara = 16.67;//Microsegundo
-const float Encendido = 3;//Microsegundo
+const float Dispara = 8.33;
+const float Encendido = 2;
 
 int Salida1 = 7;
 int Salida2 = 6;
 
+int veces = 0;
 int Interupcion = 0;//Pin 2
 int Potenciometro = A0;
 float Retardo = 0;
 float Tiempo = 0;
 
 void setup() {
-  Serial.begin(9600);
   delay(300);
   lcd.begin(16, 2);
   lcd.clear();
@@ -24,14 +23,11 @@ void setup() {
   pinMode(Salida1, OUTPUT);
   pinMode(Salida2, OUTPUT);
   pinMode(Potenciometro, INPUT);
-  attachInterrupt(Interupcion, MandarPulso  , RISING );
+  attachInterrupt(Interupcion, MandarPulso, RISING );
 }
 
 void loop() {
   Retardo = map(analogRead(Potenciometro), 0, 1023, Dispara, 0);
-  Retardo =  constrain(Retardo, 0, Dispara);
-  Serial.print("Retardo ");
-  Serial.println(Retardo);
   int Angulo = map(Retardo, 0, Dispara, 0, 360);
   lcd.setCursor(0, 0);
   lcd.print("Angulo ");
