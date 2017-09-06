526bac235b92b94536524c939e5bf19d583f586f
chepecarlos <chepecarlos@alswblog.org>
2017-09-06 17:39:21 -0600

    Mejorando codigo

--- leslie/leslie.ino
+++ leslie/leslie.ino
@@ -2,11 +2,9 @@
 #include <SoftwareSerial.h> //Requerida internamete por el escudo WiFi
 #include <SparkFunESP8266WiFi.h>
 
-
 //Configuracion de la red inalambrica
-const char ssid[] = "TURBONETT_ALSW";
-const char password[] = "2124-1324";
-
+const char ssid[] = "Manuel";
+const char password[] = "12345678";
 
 //Cadena usada para almacenar la ruta de la URL
 String rutaUrl;
@@ -15,20 +13,19 @@ String rutaUrl;
 ESP8266Server server = ESP8266Server(80);
 ESP8266Client cliente;
 
-char contrasena[1] = "1111"; //aquí escribimos la contraseña de 4 dígitos // contraseña para encender las luces
-char contrasena2[] = "2222"; //contraseña para desactivar la alarma
-char contrasena3[] = "3333"; // Contraseña para activar la alarma
+char contrasena[5][4] = {"0000", "1111", "2222", "3333", "4444"}; //aquí escribimos la contraseña de 4 dígitos // contraseña para encender las luces
+char contrasenaDesactivar[4] = "1234"; //contraseña para desactivar la alarma
+char ContrasenaActivar[4] = "4321"; // Contraseña para activar la alarma
 char codigo[4];            //Cadena donde se guardaran los caracteres d0e las teclas presionadas
 int cont = 0;        //variable que se incrementara al presionar las teclas
-int inPin = 10;
-int Buzzer = 12;
+const int Pir[2] = {10, 11};
+const int Buzzer = 12;
 const int LedEstado = 13;
 const byte ROWS = 4; //Numero de filas del teclado que se esta usando
 const byte COLS = 4; //Numero de columnas del teclado que se esta usando
-const int Luces_Conference1 = A0;
-const int Luces_Conference2 = 3 ;
-const int Luces_Entrada = 4 ;
-const int Luces_Oficina = 5 ;
+const int Led[4] = {A0, A1, A2, A3};
+boolean EstadoLed[4] = {false, false, false, false};
+int EstadoAlarma = 0;
 char hexaKeys[ROWS][COLS] =  //Aquí pondremos la disposición de los caracteres tal cual están en nuestro teclado
 {
   {'1', '2', '3', 'A'},
@@ -37,47 +34,58 @@ char hexaKeys[ROWS][COLS] =  //Aquí pondremos la disposición de los caracteres
   {'*', '0', '#', 'D'}
 };
 
-byte rowPins[ROWS] = {9, 8, 7, 6}; //Seleccionamos los pines en el arduino donde iran conectadas las filas
-byte colPins[COLS] = {5, 4, 3, 2}; //Seleccionamos los pines en el arduino donde iran conectadas las columnas
+byte rowPins[ROWS] = {3, 2, 1, 0}; //Seleccionamos los pines en el arduino donde iran conectadas las filas
+byte colPins[COLS] = {7, 6, 5, 4}; //Seleccionamos los pines en el arduino donde iran conectadas las columnas
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); //inicializa el teclado
 
 void setup()
 {
-  Serial.begin(9600); //inicializar puerto serie
-  pinMode(inPin, INPUT);
-  pinMode (Luces_Conference1, OUTPUT);
-  pinMode (Luces_Conference2, OUTPUT);
-  pinMode (LedEstado, OUTPUT);
-  pinMode (Luces_Entrada, OUTPUT);
-  pinMode (Luces_Oficina, OUTPUT);
-  pinMode(Buzzer, OUTPUT);
+  IPAddress direccionIP;
 
+  Serial.begin(9600); //inicializar puerto serie
+
+  Serial.println("Iniciando");
+  for (int i = 0; i < 4; i++) {
+    pinMode (Led[i], OUTPUT);
+  }
+  pinMode (LedEstado, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
+  for (int i = 0; i < 2; i++) {
+    pinMode (Pir[i], INPUT);
+  }
 
   //Inicializa el modulo WiFi
+  Serial.println("Activiando Wifi");
   esp8266.begin();
   esp8266.setMode(ESP8266_MODE_STA);  //Modo estacion (cliente WiFi)
-  while (esp8266.connect(ssid, password) <= 0) {
-    //Si no se puede conectar emite un error y para el programa
-    Serial.println(F("Reintentando"));
-    digitalWrite(LedEstado, 1);
-    delay(500);
-    digitalWrite(LedEstado, 0);
-    delay(500);
+
+
+  if (esp8266.status() <= 0) // If we're not already connected
+  {
+    while (esp8266.connect(ssid, password) < 0)
+    {
+      Serial.println(F("Error connecting"));
+      digitalWrite(LedEstado, 1);
+      delay(250);
+      digitalWrite(LedEstado, 0);
+      delay(250);
+    }
   }
   //Comienza a servir la pagina web
   server.begin();
 
   //Se lee la direccion IP y se indica al usuario a donde ingresar
-  direccionIP = esp8266.localIP();
   Serial.print(F("Servidor listo, visite la direccion "));
-  Serial.println(direccionIP);
+  Serial.println(esp8266.localIP());
 
 }
 
 void loop()
 {
   Teclado();
+  EstadoPir();
+  ActualizarLed();
   ConsultaWifi();
 }
 
@@ -127,18 +135,15 @@ void procesarCliente() {
         //Dependiendo de la URL solicitada, se enciende o apaga el LED
         //(Nota: Si no se especifica ninguna ruta valida, no se hace
         //nada con el LED)
-        if (rutaUrl == "/Luces_Conference1") {
-          //Codigo Luces_Conference1
+        if (rutaUrl == "/Led0E") {
+          EstadoLed[0] = true;
+          Serial.println("Led 1 Encender");
         }
-        else if (rutaUrl == "/Luces_Conference2") {
-          //Codigo Luces_Conference2
+        else if (rutaUrl == "/Led0P") {
+          EstadoLed[0] = false;
+          Serial.println("Led 1 Apagado");
         }
-        else if (rutaUrl == "/Luces_Entrada") {
-          //Codigo Luces_Entrada
-        }
-        if (rutaUrl == "/Luces_Oficina") {
-          //Codigo Luces_Oficina
-        }
+
 
         //Despues de la solicitud siempre se pasa al siguiente estado
         estado = 2;
@@ -182,23 +187,49 @@ void enviarPagina() {
                               "Connection: close\r\n\r\n";
   const char cabeceraHTML[] = "<!DOCTYPE html>\n"
                               "<html>\n"
-                              "  Estado del LED: ";
-  const char terminadorHTML_0[] = "Apagado \n"
-                                  "  <br> <a href=\"/encender\"> Encender </a>\n"
-                                  "</html>\n";
-  const char terminadorHTML_1[] = "Encendido \n"
-                                  "  <br> <a href=\"/apagar\"> Apagar </a>\n"
-                                  "</html>\n";
+                              "  <h1>Sistema Leslie </h1> ";
+  const char TextoApagado[] = " Apagado ";
+  const char TextoEncender[] = " Encendido ";
+
+  const char Nombre0[] = "Conferencia 1: ";
+  const char Nombre1[] = "Conferencia 2: ";
+  const char Nombre2[] = "Oficina 1: ";
+  const char Nombre3[] = "Entrada ";
+
+  const char Numero0[] = "0";
+
+  const char EstadoActivo[] = "E";
+  const char EstadoApagado[] = "P";
+
+  const char TextoA[] = " Link: <a href=\"/Led";
+
+  const char TextoFinA[] = "\">";
+  const char TextoCierre[] = " </a><br>\n";
+  const char TextoFinHTML[] = "</html>\n";
 
   //El inicio de la pagina siempre es el mismo
   cliente.write(cabeceraHTTP, strlen(cabeceraHTTP));
   cliente.write(cabeceraHTML, strlen(cabeceraHTML));
 
   //El final de la pagina depende del estado del LED
-  if (!estadoLed)
-    cliente.write(terminadorHTML_0, strlen(terminadorHTML_0));
-  else
-    cliente.write(terminadorHTML_1, strlen(terminadorHTML_1));
+  if (EstadoLed[0] == true) {
+    cliente.write(Nombre0, strlen(Nombre0));
+    cliente.write(TextoEncender, strlen(TextoEncender));
+    cliente.write(TextoA, strlen(TextoA));
+    cliente.write(Numero0, strlen(Numero0));
+    cliente.write(EstadoApagado, strlen(EstadoApagado));
+    cliente.write(TextoFinA, strlen( TextoFinA));
+    cliente.write(TextoApagado, strlen(TextoApagado));
+    cliente.write(TextoCierre, strlen(TextoCierre));
+  }
+  else if (EstadoLed[0] == true) {
+    //Les apgado y a enceder
+
+  }
+  if (EstadoLed[1] == true) {
+    //hola
+  }
+  cliente.write(TextoFinHTML, strlen(TextoFinHTML));
 }
 
 void Teclado() {
@@ -206,49 +237,79 @@ void Teclado() {
   char customKey = customKeypad.getKey(); //se guarda en la variable customKey el caracter de la tecla presionada
   if (customKey != NO_KEY)         //se evalúa si se presionó una tecla
   {
+    //Leer Codigo
     codigo[cont] = customKey;        //se guarda caracter por caracter en el arreglo codigo[]
     Serial.println(codigo[cont]);    //se imprime en el puerto serie la tecla presionada
-    cont = cont + 1;          //incrementamos la variable cont
-    if (cont == 4)       //si ya fueron presionadas 4 teclas se evalúa si la contraseña es correcta
-    {
-      if (codigo[0] == contrasena[0] && codigo[1] == contrasena[1] && codigo[2] == contrasena[2] && codigo[3] == contrasena[3])
-      {
-        digitalWrite(13, !digitalRead(13));       //si la contraseña fué correcta se enciende o se apaga el led del pin13
-        digitalWrite(Luces_Conference1, !digitalRead(Luces_Conference1));
-        digitalWrite(Luces_Conference2, !digitalRead(Luces_Conference2));
-        digitalWrite(Luces_Entrada, !digitalRead(Luces_Entrada));
-        digitalWrite(Luces_Oficina, !digitalRead(Luces_Oficina));
-      }
-      cont = 0; //resetear a 0 la variable cont
-    }
-  }
-  if (digitalRead(inPin) == HIGH && !(codigo[0] == contrasena[0] && codigo[1] == contrasena[1] && codigo[2] == contrasena[2] && codigo[3] == contrasena[3])) {
-    for (int i = 0; i <= 4; i++) {
-      tone(Buzzer, 2700, 250);
-      digitalWrite(13, HIGH);
-      // delay(100);
-      // digitalWrite(13, LOW);
-      // digitalWrite(Luces_Conference1, HIGH);
-      // delay (100);
-      // digitalWrite(Luces_Conference2, HIGH);
-    }
-  }
-  else if (codigo[0] == contrasena2[0] && codigo[1] == contrasena2[1] && codigo[2] == contrasena2[2] && codigo[3] == contrasena2[3]) {
-    //digitalWrite(Luces_Conference1 , LOW);
-    //digitalWrite(Luces_Conference2, LOW);
-    digitalWrite(11, HIGH);
-  }
-  if (digitalRead(11) == HIGH)
-  {
-    digitalWrite(13, LOW);
-  }
-  if (codigo[0] == contrasena3[0] && codigo[1] == contrasena3[1] && codigo[2] == contrasena3[2] && codigo[3] == contrasena3[3] || codigo[0] == contrasena[0] && codigo[1] == contrasena[1] && codigo[2] == contrasena[2] && codigo[3] == contrasena[3]) {
-    digitalWrite(11, LOW);
-  }
-  //if(codigo[0]==contrasena2[0]&&codigo[1]==contrasena2[1]&&codigo[2]==contrasena2[2]&&codigo[3]==contrasena2[3]){
-  //    digitalWrite(Luces_Conference1 , LOW);
-  //      digitalWrite(Luces_Conference2, LOW);
-  //        delay(100);               // esperar 1 segundos
-  //}
+    cont = cont + 1;
+    if (cont >= 4)
+      cont = 0;
 
+    //Cambiar LED
+    if ( EstadoAlarma == 0 ) {
+      for (int micu = 0; micu < 5; micu++) {
+        boolean SonIgual = true;
+        for (int i = 0; i < 4; i++) {
+          if (codigo[i] != contrasena[micu][i])
+            SonIgual = false;
+        }
+        if (SonIgual && micu < 4) {
+          EstadoLed[micu] = !EstadoLed[micu];
+        }
+        else if (SonIgual && micu == 4) {
+          for (int i = 0; i < 4; i++) {
+            EstadoLed[i] = !EstadoLed[i];
+          }
+        }
+      }
+    }
+    //Desactivar alarma
+    boolean IgualDesactivar = true;
+    for (int i = 0; i < 4; i++) {
+      if (codigo[i] !=  contrasenaDesactivar[i])
+        IgualDesactivar = false;
+    }
+    if (IgualDesactivar) {
+      EstadoAlarma = 0;
+    }
+    //Activar Alarma
+    boolean IgualActivar = true;
+    for (int i = 0; i < 4; i++) {
+      if (codigo[i] !=  contrasenaDesactivar[i])
+        IgualActivar = false;
+    }
+    if (IgualActivar) {
+      EstadoAlarma = 1;
+    }
+  }
 }
+void ActualizarLed() {
+  if ( EstadoAlarma == 2) {
+    tone(Buzzer, 800);
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(Led[i], EstadoLed[i]);
+      EstadoLed[i] = !EstadoLed[i];
+    }
+    //delay(200);
+  }
+  else if (EstadoAlarma == 1) {
+
+  }
+  else if (EstadoAlarma == 0) {
+    noTone(Buzzer);
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(Led[i], EstadoLed[i]);
+    }
+  }
+}
+
+void EstadoPir() {
+  if (EstadoAlarma = 1) {
+    for (int i = 0; i < 2; i++) {
+      int EstadoPir = digitalRead(Pir[i]);
+      if (EstadoPir == 1) {
+        EstadoAlarma = 2;
+      }
+    }
+  }
+}
+
