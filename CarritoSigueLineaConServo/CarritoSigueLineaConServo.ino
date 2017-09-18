e8360bc10cc8d5e5f025aef2af5886bd45b4d20f
Carl0sHdz <carlitos.hernandez203@gmail.com>
2017-09-18 16:44:32 -0600

    Proyecto casi terminado

--- CarritoSigueLineaConServo/CarritoSigueLineaConServo.ino
+++ CarritoSigueLineaConServo/CarritoSigueLineaConServo.ino
@@ -4,55 +4,74 @@ int Motor2D = 6;
 int Motor1I = 8;
 int Motor2I = 9;
 
-int sensor1 = 0;
-int sensor2 = 0;
+int SensorI = 0;
+int SensorD = 0;
+
+int vcc1 = 2;
+int vcc2 = 3;
+int gnd1 = 11;
+int gnd2 = 12;
 void setup() {
-  pinMode(Motor1D,OUTPUT);
-  pinMode(Motor2D,OUTPUT);
-  pinMode(Motor1I,OUTPUT);
-  pinMode(Motor2I,OUTPUT);
+  pinMode(vcc1, OUTPUT);
+  pinMode(vcc2, OUTPUT);
+  pinMode(gnd1, OUTPUT);
+  pinMode(gnd2, OUTPUT);
+  digitalWrite(vcc1, HIGH);
+  digitalWrite(vcc2, HIGH);
+  digitalWrite(gnd1, LOW);
+  digitalWrite(gnd2, LOW);
+  pinMode(Motor1D, OUTPUT);
+  pinMode(Motor2D, OUTPUT);
+  pinMode(Motor1I, OUTPUT);
+  pinMode(Motor2I, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  delante();
-  delay(2000);
-  alto();
-  delay(2000);
-  derecha();
-  delay(2000);
-  alto();
-  delay(2000);
-  izquierda();
-  delay(2000);
-  alto();
-  delay(2000);
+  SensorI  = analogRead(A0);
+  SensorD = analogRead(A2);
+  if (SensorD < 500 && SensorI < 500) {
+    alto();
+  }
+  else if (SensorD > 500 && SensorI > 500) {
+    delante();
+  }
+  else if (SensorD > 500 && SensorI < 500) {
+    derecha();
+  }
+  else if (SensorD < 500 && SensorI > 500) {
+    izquierda();
+  }
+  else {
+    alto();
+  }
+  delay(1);
 }
 
 void alto() {
-  digitalWrite(Motor2D,LOW);
-  digitalWrite(Motor1D,LOW);
-  digitalWrite(Motor1I,LOW);
-  digitalWrite(Motor2I,LOW);
+  digitalWrite(Motor2D, LOW);
+  digitalWrite(Motor1D, LOW);
+  digitalWrite(Motor1I, LOW);
+  digitalWrite(Motor2I, LOW);
 }
 
 void delante() {
-  digitalWrite(Motor2D,HIGH);
-  digitalWrite(Motor1D,LOW);
-  digitalWrite(Motor1I,HIGH);
-  digitalWrite(Motor2I,LOW);
+  digitalWrite(Motor2D, HIGH);
+  digitalWrite(Motor1D, LOW);
+  digitalWrite(Motor1I, HIGH);
+  digitalWrite(Motor2I, LOW);
 }
 
 void izquierda() {
-  digitalWrite(Motor2D,LOW);
-  digitalWrite(Motor1D,HIGH);
-  digitalWrite(Motor1I,HIGH);
-  digitalWrite(Motor2I,LOW);
+  digitalWrite(Motor2D, HIGH);
+  digitalWrite(Motor1D, LOW);
+  digitalWrite(Motor1I, LOW);
+  digitalWrite(Motor2I, HIGH);
 }
 
 void derecha() {
-  digitalWrite(Motor2D,HIGH);
-  digitalWrite(Motor1D,LOW);
-  digitalWrite(Motor1I,LOW);
-  digitalWrite(Motor2I,HIGH);
+  digitalWrite(Motor2D, LOW);
+  digitalWrite(Motor1D, HIGH);
+  digitalWrite(Motor1I, HIGH);
+  digitalWrite(Motor2I, LOW);
 }
