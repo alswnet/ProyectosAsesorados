be61f387bcfb6396c875fd602196af4afc5b1811
chepecarlos <chepecarlos@alswblog.org>
2017-09-21 19:29:10 -0600

    Codigo lindo y funciona

--- Llenado/Llenado.ino
+++ Llenado/Llenado.ino
@@ -3,41 +3,30 @@ int sensor2 = 25;
 int sensor3 = 26;
 int PinInicio = 42;
 int ValorInicio = 0;
-int STOP = 41;
 int EV06 = 38;
 int EV05 = 37;
 int EV04 = 34;
 int M03 = 33;
-int K02 = 30;
 int banda = 29;
-int ledprueba = 13;
 //declaración de pines para las entradas y salidas
-int botonA = 0;
-int botonB = 0;
 int sensorA = 0;
 int sensorB = 0;
 int sensorC = 0;
-int lecturaStart = 0;
-int lecturaSensor1 = 0;
-
+int Estado = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode (sensor1, INPUT);
-  pinMode (sensor2, INPUT);
-  pinMode (sensor3, INPUT);
+  pinMode (sensor1, INPUT_PULLUP);
+  pinMode (sensor2, INPUT_PULLUP);
+  pinMode (sensor3, INPUT_PULLUP);
   pinMode (PinInicio, INPUT_PULLUP);
-  pinMode (STOP, INPUT);
   pinMode (EV06, OUTPUT);
   pinMode (EV05, OUTPUT);
   pinMode (EV04, OUTPUT);
   pinMode (M03, OUTPUT);
-  pinMode (K02, OUTPUT);
   pinMode (banda, OUTPUT);
 
 
-
-  // paso 0
 
 }
 
@@ -48,176 +37,88 @@ void loop() {
   sensorC = digitalRead(sensor3);
   ValorInicio = digitalRead(PinInicio);
 
-  Serial.print("El boton esta ");
-  Serial.println(ValorInicio);
+  Serial.print("Valor A:");
+  Serial.print(sensorA);
 
-  if (ValorInicio == 1) {
+  Serial.print(" Valor B:");
+  Serial.print(sensorB);
 
+  Serial.print(" Valor C:");
+  Serial.println(sensorC);
 
-
-    lecturaStart = Serial.read();
-    //lecturaSensor1 = Serial.read();
-    //Serial.println("usb conectado");
-    //delay(3000);
+  if (ValorInicio == 0) {
+    digitalWrite(banda, LOW);
+    digitalWrite(M03, LOW);
+    digitalWrite(EV05, LOW);
+    digitalWrite(EV04, LOW);
+    digitalWrite(EV06, LOW);
+    Estado = 0;
   }
 
-  digitalWrite(banda, HIGH);
 
-  if (sensorA == 1)
-  {
-    digitalWrite(banda, LOW);
-    digitalWrite(EV04, HIGH); //pistón
-    delay (2000);
-    digitalWrite(EV05, HIGH); //lvula liquido
-    delay (2000);
-    digitalWrite(M03, HIGH); //bomba
-    delay(5000);
+  switch (Estado) {
+    case 0:
+      // Apagada y esperadon boton encendido
+      Serial.println("E0-Apagada");
+      if (ValorInicio == 1) {
+        digitalWrite(banda, HIGH);
+        Estado = 1;
+      }
+      break;
+    case 1:
+      // Banda Activa y esperando Senso A
+      if ( sensorA == 1) {
+        Serial.println("E1-Esperando Sensor A");
+        digitalWrite(banda, LOW);
+        Estado = 2;
+      }
+      break;
+    case 2:
+      //Llenando y despues enciende banda
+      Serial.println("E2-llenando");
+      digitalWrite(EV04, HIGH); //pistón
+      delay (2000);
+      digitalWrite(EV05, HIGH); //lvula liquido
+      delay (2000);
+      digitalWrite(M03, HIGH); //bomba
+      delay(5000);
 
-    digitalWrite(M03, LOW); //
-    delay(2000);
-    digitalWrite(EV05, LOW); //valvula liquido
-    delay(2000);
-    digitalWrite(M03, LOW); //bomba
-    delay(3000);
-    digitalWrite(banda, HIGH);
-  }
-
-  //   {
-  //    digitalWrite(banda,LOW);
-  //    digitalWrite(EV04, LOW); //pistón
-  //    digitalWrite(EV05, LOW); //valvula liquido
-  //    digitalWrite(M03, LOW); //bomba
-  //    delay(5000);
-
-  // }
-
-
-  if (sensorB == 1)
-  {
-
-    digitalWrite(banda, LOW);
-    delay(3000);
-    digitalWrite(EV06, HIGH); //taponado
-    delay(4000);
-    digitalWrite(EV06, LOW); //taponado
-    delay(3000);
-    digitalWrite(banda, HIGH);
-  }
-
-  if (sensorC == 1)
-  {
-    digitalWrite(banda, LOW);
-
+      digitalWrite(M03, LOW); //
+      delay(2000);
+      digitalWrite(EV05, LOW); //valvula liquido
+      delay(2000);
+      digitalWrite(EV04, LOW); //bomba
+      delay(3000);
+      digitalWrite(banda, HIGH);
+      Estado = 3;
+    case 3:
+      //Esperando sensor B si apaga la banda
+      Serial.println("E3-Esperando Sensor B");
+      if ( sensorB == 1) {
+        digitalWrite(banda, LOW);
+        Estado = 4;
+      }
+      break;
+    case 4:
+      //Dandole un poraso a la Tapando
+      Serial.println("E4-Tapando");
+      digitalWrite(banda, LOW);
+      delay(3000);
+      digitalWrite(EV06, HIGH); //taponado
+      delay(4000);
+      digitalWrite(EV06, LOW); //taponado
+      delay(3000);
+      digitalWrite(banda, HIGH);
+      Estado = 5;
+      break;
+    case 5:
+      //Esperando el Sensor C
+      Serial.println("E5-Esperando Sensor C");
+      if ( sensorC == 1) {
+        digitalWrite(banda, LOW);
+        delay(6000);
+        Estado = 0;
+      }
+      break;
   }
 }
-
-
-
-
-
-
-
-/*if(sensorA == 1)
-  {
-  digitalWrite(banda, LOW);
-
-  digitalWrite(EV04, HIGH); //pistón0
-  digitalWrite(M03, HIGH); //bomba
-  digitalWrite(EV05, HIGH); //valvula liquido
-  delay(8000);
-
-  digitalWrite(banda, HIGH);
-  }
-
-  else
-  {
-  digitalWrite(banda, HIGH);
-  }
-
-  if(sensorB == 1)
-  {
-  digitalWrite(banda, LOW);
-  digitalWrite(EV06, HIGH); //pistón
-  delay(8000);
-
-  digitalWrite(banda, HIGH); //bomba
-
-  }
-
-  else
-  {
-  digitalWrite(banda, HIGH);
-  }
-
-  if(sensorC == 1)
-  {
-  digitalWrite(banda, LOW);
-  }
-
-  else
-  {
-  digitalWrite(banda, HIGH);
-  }
-
-
-
-  }
-
-
-
-
-
-  /*}
-  do{
-  if (sensorA == HIGH ){
-  digitalWrite(banda,LOW);
-  delay(50);
-  digitalWrite(EV04, HIGH);
-  delay(2000);
-  digitalWrite(EV05,HIGH);
-  digitalWrite(M03,HIGH);
-  delay (5000) ;
-  digitalWrite(M03,LOW);
-  digitalWrite(EV05,LOW);
-  delay (800);
-  digitalWrite(EV04,LOW);
-  digitalWrite(banda, HIGH);
-  delay(5000);
-  digitalWrite(banda, HIGH);
-  }
-
-  else {
-  digitalWrite(banda, HIGH);
-  }
-
-
-  // paso 2
-  if (sensorB == HIGH ) {
-  digitalWrite(banda, LOW);
-  delay (1000);
-  digitalWrite(EV06,HIGH);
-  delay (4000);
-  digitalWrite(K02,HIGH);
-  delay (3000);
-  digitalWrite(K02,LOW);
-  delay (4000);
-  digitalWrite(EV06,LOW);
-  digitalWrite(banda, HIGH);
-  delay (5000);
-  digitalWrite(banda, HIGH);
-  }
-  else {
-  digitalWrite(banda, HIGH);
-  }
-
-  // paso 3
-  if (sensorC == HIGH) {
-  digitalWrite(banda, LOW);
-  }
-  else {
-  digitalWrite(banda, HIGH);
-  }
-  delay(1000);*/
-
-
-
