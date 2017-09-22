45a31a278852234c0b7798d9abf327acfb279e28
chepecarlos <chepecarlos@alswblog.org>
2017-09-21 18:21:59 -0600

    Funciona supuestamente

--- Llenado/Llenado.ino
+++ Llenado/Llenado.ino
@@ -1,7 +1,8 @@
 int sensor1 = 22;
 int sensor2 = 25;
 int sensor3 = 26;
-int START = 42;
+int PinInicio = 42;
+int ValorInicio = 0;
 int STOP = 41;
 int EV06 = 38;
 int EV05 = 37;
@@ -21,39 +22,38 @@ int lecturaSensor1 = 0;
 
 
 void setup() {
-    Serial.begin(9600);
-pinMode (sensor1,INPUT);
-pinMode (sensor2,INPUT);
-pinMode (sensor3,INPUT);
-pinMode (START,INPUT_PULLUP);
-pinMode (STOP,INPUT);
-pinMode (EV06,OUTPUT);
-pinMode (EV05,OUTPUT);
-pinMode (EV04,OUTPUT);
-pinMode (M03,OUTPUT);
-pinMode (K02,OUTPUT);
-pinMode (banda,OUTPUT);
+  Serial.begin(9600);
+  pinMode (sensor1, INPUT);
+  pinMode (sensor2, INPUT);
+  pinMode (sensor3, INPUT);
+  pinMode (PinInicio, INPUT_PULLUP);
+  pinMode (STOP, INPUT);
+  pinMode (EV06, OUTPUT);
+  pinMode (EV05, OUTPUT);
+  pinMode (EV04, OUTPUT);
+  pinMode (M03, OUTPUT);
+  pinMode (K02, OUTPUT);
+  pinMode (banda, OUTPUT);
 
 
 
-// paso 0
-   
+  // paso 0
+
 }
-  
+
 void loop() {
 
-  
-  {
+  sensorA = digitalRead(sensor1);
+  sensorB = digitalRead(sensor2);
+  sensorC = digitalRead(sensor3);
+  ValorInicio = digitalRead(PinInicio);
 
-   sensorA = digitalRead(sensor1);
-   sensorB = digitalRead(sensor2);
-   sensorC = digitalRead(sensor3);
-    //Serial.println(START);
-   // Serial.println(sensorB);
-    Serial.println(sensorC);}
-  if (Serial.available()){
+  Serial.print("El boton esta ");
+  Serial.println(ValorInicio);
 
-   
+  if (ValorInicio == 1) {
+
+
 
     lecturaStart = Serial.read();
     //lecturaSensor1 = Serial.read();
@@ -61,162 +61,162 @@ void loop() {
     //delay(3000);
   }
 
-      digitalWrite(banda, HIGH);
-     
-     if(sensorA == 1)
-     {
-        digitalWrite(banda,LOW);
-        digitalWrite(EV04, HIGH); //pistón
-        delay (2000);
-        digitalWrite(EV05,HIGH);//lvula liquido
-        delay (2000);
-        digitalWrite(M03, HIGH); //bomba
-        delay(5000);
-        
-        digitalWrite(M03, LOW); // 
-        delay(2000);
-        digitalWrite(EV05,LOW);//valvula liquido
-        delay(2000);
-        digitalWrite(M03, LOW); //bomba
-        delay(3000);
-        digitalWrite(banda,HIGH);
-        }
-      
-  //   {
-    //    digitalWrite(banda,LOW);
-    //    digitalWrite(EV04, LOW); //pistón
-    //    digitalWrite(EV05, LOW); //valvula liquido
-    //    digitalWrite(M03, LOW); //bomba
-    //    delay(5000);
-        
-    // }
-     
+  digitalWrite(banda, HIGH);
 
-      if (sensorB == 1) 
-      {
-      
-      digitalWrite(banda, LOW);
-      delay(3000);
-      digitalWrite(EV06,HIGH); //taponado
-      delay(4000);
-      digitalWrite(EV06,LOW); //taponado
-      delay(3000);
-      digitalWrite(banda, HIGH);
-      }
-
-       if(sensorC == 1)
-     {
-        digitalWrite(banda, LOW);
-    
-     }
-  }
-
-     
-
-     
-  
-  
-    
-      /*if(sensorA == 1)
-        {
-          digitalWrite(banda, LOW);
-
-          digitalWrite(EV04, HIGH); //pistón0
-          digitalWrite(M03, HIGH); //bomba
-          digitalWrite(EV05, HIGH); //valvula liquido
-          delay(8000);
-
-          digitalWrite(banda, HIGH);
-        }
-
-        else
-        {
-          digitalWrite(banda, HIGH);
-        }
-
-      if(sensorB == 1)
-      {
-          digitalWrite(banda, LOW);
-          digitalWrite(EV06, HIGH); //pistón
-          delay(8000);
-          
-          digitalWrite(banda, HIGH); //bomba
-        
-      }
-
-      else
-      {
-        digitalWrite(banda, HIGH);
-      }
-
-      if(sensorC == 1)
-      {
-        digitalWrite(banda, LOW);
-      }
-
-      else
-      {
-        digitalWrite(banda, HIGH);
-      }
-      
-
-          
-        }
-        
-  
-
-  
-  
-  /*}
- do{   
-  if (sensorA == HIGH ){
-    digitalWrite(banda,LOW);
-    delay(50);
-    digitalWrite(EV04, HIGH);
-    delay(2000);
-    digitalWrite(EV05,HIGH);
-    digitalWrite(M03,HIGH);
-    delay (5000) ;
-    digitalWrite(M03,LOW);
-    digitalWrite(EV05,LOW);
-    delay (800);
-    digitalWrite(EV04,LOW);
-   digitalWrite(banda, HIGH);
+  if (sensorA == 1)
+  {
+    digitalWrite(banda, LOW);
+    digitalWrite(EV04, HIGH); //pistón
+    delay (2000);
+    digitalWrite(EV05, HIGH); //lvula liquido
+    delay (2000);
+    digitalWrite(M03, HIGH); //bomba
     delay(5000);
-    digitalWrite(banda, HIGH);
-  }
-  
-else {
-    digitalWrite(banda, HIGH);
-  } 
 
-  
+    digitalWrite(M03, LOW); //
+    delay(2000);
+    digitalWrite(EV05, LOW); //valvula liquido
+    delay(2000);
+    digitalWrite(M03, LOW); //bomba
+    delay(3000);
+    digitalWrite(banda, HIGH);
+  }
+
+  //   {
+  //    digitalWrite(banda,LOW);
+  //    digitalWrite(EV04, LOW); //pistón
+  //    digitalWrite(EV05, LOW); //valvula liquido
+  //    digitalWrite(M03, LOW); //bomba
+  //    delay(5000);
+
+  // }
+
+
+  if (sensorB == 1)
+  {
+
+    digitalWrite(banda, LOW);
+    delay(3000);
+    digitalWrite(EV06, HIGH); //taponado
+    delay(4000);
+    digitalWrite(EV06, LOW); //taponado
+    delay(3000);
+    digitalWrite(banda, HIGH);
+  }
+
+  if (sensorC == 1)
+  {
+    digitalWrite(banda, LOW);
+
+  }
+}
+
+
+
+
+
+
+
+/*if(sensorA == 1)
+  {
+  digitalWrite(banda, LOW);
+
+  digitalWrite(EV04, HIGH); //pistón0
+  digitalWrite(M03, HIGH); //bomba
+  digitalWrite(EV05, HIGH); //valvula liquido
+  delay(8000);
+
+  digitalWrite(banda, HIGH);
+  }
+
+  else
+  {
+  digitalWrite(banda, HIGH);
+  }
+
+  if(sensorB == 1)
+  {
+  digitalWrite(banda, LOW);
+  digitalWrite(EV06, HIGH); //pistón
+  delay(8000);
+
+  digitalWrite(banda, HIGH); //bomba
+
+  }
+
+  else
+  {
+  digitalWrite(banda, HIGH);
+  }
+
+  if(sensorC == 1)
+  {
+  digitalWrite(banda, LOW);
+  }
+
+  else
+  {
+  digitalWrite(banda, HIGH);
+  }
+
+
+
+  }
+
+
+
+
+
+  /*}
+  do{
+  if (sensorA == HIGH ){
+  digitalWrite(banda,LOW);
+  delay(50);
+  digitalWrite(EV04, HIGH);
+  delay(2000);
+  digitalWrite(EV05,HIGH);
+  digitalWrite(M03,HIGH);
+  delay (5000) ;
+  digitalWrite(M03,LOW);
+  digitalWrite(EV05,LOW);
+  delay (800);
+  digitalWrite(EV04,LOW);
+  digitalWrite(banda, HIGH);
+  delay(5000);
+  digitalWrite(banda, HIGH);
+  }
+
+  else {
+  digitalWrite(banda, HIGH);
+  }
+
+
   // paso 2
-   if (sensorB == HIGH ) {
-    digitalWrite(banda, LOW);
-    delay (1000);
-    digitalWrite(EV06,HIGH);
-    delay (4000);
-    digitalWrite(K02,HIGH);
-    delay (3000);
-    digitalWrite(K02,LOW);
-    delay (4000);
-    digitalWrite(EV06,LOW);
-    digitalWrite(banda, HIGH);
-    delay (5000);
-    digitalWrite(banda, HIGH);
+  if (sensorB == HIGH ) {
+  digitalWrite(banda, LOW);
+  delay (1000);
+  digitalWrite(EV06,HIGH);
+  delay (4000);
+  digitalWrite(K02,HIGH);
+  delay (3000);
+  digitalWrite(K02,LOW);
+  delay (4000);
+  digitalWrite(EV06,LOW);
+  digitalWrite(banda, HIGH);
+  delay (5000);
+  digitalWrite(banda, HIGH);
   }
   else {
-    digitalWrite(banda, HIGH);
+  digitalWrite(banda, HIGH);
   }
-   
-   // paso 3
+
+  // paso 3
   if (sensorC == HIGH) {
-    digitalWrite(banda, LOW);
+  digitalWrite(banda, LOW);
   }
   else {
-    digitalWrite(banda, HIGH);
-  } 
+  digitalWrite(banda, HIGH);
+  }
   delay(1000);*/
 
 
