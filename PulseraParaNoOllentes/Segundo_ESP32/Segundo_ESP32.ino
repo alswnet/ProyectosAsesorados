e79d73b2076331486f5c9ddc4dca99110b97a23f
Ozyamandias <hedeleon09@gmail.com>
2019-07-29 16:56:18 -0600

    Se hizo el segundo ESP

--- PulseraParaNoOllentes/Segundo_ESP32/Segundo_ESP32.ino
+++ PulseraParaNoOllentes/Segundo_ESP32/Segundo_ESP32.ino
@@ -1,14 +1,15 @@
-
 #include <WiFi.h>
 #include <MQTT.h>
 
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 const char ssid[] = "ALSW";
 const char pass[] = "25264897";
+
+long distancia;
+long tiempo;
+
+int Trigger = 13;
+int Echo = 12;
 
 WiFiClient net;
 MQTTClient client;
@@ -36,19 +37,15 @@ void connect() {
 
 void messageReceived(String &topic, String &payload) {
   Serial.println("incoming: " + topic + " - " + payload);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(payload);
-  delay(5000);
-  lcd.clear();
 }
 
 void setup() {
-  lcd.init();
-  lcd.backlight();
 
   Serial.begin(115200);
   WiFi.begin(ssid, pass);
+
+  pinMode(Trigger, OUTPUT);
+  pinMode(Echo,INPUT);
 
   // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
   // You need to set the IP address directly.
@@ -59,14 +56,30 @@ void setup() {
 }
 
 void loop() {
+
+  digitalWrite(Trigger,LOW); /* Por cuestión de estabilización del sensor*/
+  delayMicroseconds(5);
+  digitalWrite(Trigger, HIGH); /* envío del pulso ultrasónico*/
+  delayMicroseconds(10);
+  tiempo=pulseIn(Echo, HIGH); /* Función para medir la longitud del pulso entrante. Mide el tiempo que transcurrido entre el envío
+  del pulso ultrasónico y cuando el sensor recibe el rebote, es decir: desde que el pin 12 empieza a recibir el rebote, HIGH, hasta que
+  deja de hacerlo, LOW, la longitud del pulso entrante*/
+  distancia= int(0.017*tiempo); /*fórmula para calcular la distancia obteniendo un valor entero*/
+  /*Monitorización en centímetros por el monitor serial*/
+  Serial.println("Distancia ");
+  Serial.print(distancia);
+  Serial.print(" cm");
+  delay(1000);
+  
   client.loop();
   delay(10);  // <- fixes some issues with WiFi stability
 
   if (!client.connected()) {
     connect();
   }
-
-  
-  // publish a message roughly every second.
+  if(distancia<10)
+  {
+  client.publish("/pulsera/mensaje", "Alguien esta cerca");
+  }
 
 }
