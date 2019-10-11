5c5962d50d94b066b979f59f9378a35ad7e05c5f
chepecarlos <chepecarlos@alswblog.org>
2019-10-11 03:04:41 -0600

    Limiando codigo del braso

--- BRAZO_ROBOYICO_ITEXSAL_Braso/BRAZO_ROBOYICO_ITEXSAL_Braso.ino
+++ BRAZO_ROBOYICO_ITEXSAL_Braso/BRAZO_ROBOYICO_ITEXSAL_Braso.ino
@@ -1,8 +1,8 @@
 #include <Servo.h>
 
 int PinServo[5] = {A0, A1, A2, A3, A4};
-int Pmax[5] = {180, 180, 180, 180, 180};
-int Pmin[5] = {0, 0, 0, 0, 0};
+int Pmax[5] = {140, 160, 160, 160, 20};
+int Pmin[5] = {40, 0, 0, 20, 160};
 int Pactual[5] = {90, 90, 90, 90, 90};
 int Ppasado[5] = {90, 90, 90, 90, 90};
 Servo Dedo[5];
@@ -10,7 +10,6 @@ Servo Dedo[5];
 int VMaxSensor = 0;
 int VminSensor = 1024;
 int VactSensor = 0;
-int VanteriorSensor = -1;
 
 //Cambiar valor segun fuenta de filtro
 //Rango de 0-1
@@ -21,6 +20,7 @@ float FuerzaFiltro = 0.05;
 void setup() {
   for (int i = 0; i < 5; i++ ) {
     Dedo[i].attach(PinServo[i]);
+    Dedo[i].write(Pactual[i]);
   }
   Serial.begin(115200);
   delay(1000);
@@ -43,19 +43,15 @@ void loop() {
   }
 
   for (int i = 0; i < 5; i++) {
-    if (abs(Ppasado[i] - Pactual[i]) < 5) {
+    //if (abs(Ppasado[i] - Pactual[i]) > 5) {
       Dedo[i].write(Pactual[i]);
-      Ppasado[i] = Pactual[i];
-    }
+    //  Ppasado[i] = Pactual[i];
+    //}
   }
 
   delay(15);
 }
 
-int FiltroPasaBajo(int Valor) {
-  VanteriorSensor = FuerzaFiltro * Valor + (1 - FuerzaFiltro) * VanteriorSensor;
-  return VanteriorSensor;
-}
 
 void DecodificarSerial() {
   // Mensaje de la forma M1/30
@@ -68,8 +64,6 @@ void DecodificarSerial() {
   int Valor = Mensaje.substring(PosicionPleca + 1, PosicionSaltoLinea).toInt();
   if (Dato.equals("V")) {
     VactSensor = Valor;
-    if (VanteriorSensor < 0) {
-      VanteriorSensor = Valor;
-    }
+    Serial.println(Valor);
   }
 }
